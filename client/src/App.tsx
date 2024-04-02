85e6494795de3ff48a6ebed688b1de3bf9e6add4
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-04-02 12:25:24 +0200

    add list of icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,11 @@ import { RiSendPlane2Fill } from "react-icons/ri";
 import { socket } from "./services/seckot";
 import { scrollToBottom } from "./utils/helper";
 
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+import { FaRegFaceSmileBeam } from "react-icons/fa6";
+import { IoCloseSharp } from "react-icons/io5";
+
 const style: any = {
   send: "self-end text-xl font-medium bg-blue-500 text-white w-fit max-w-[70%] rounded-xl px-10 py-2",
   receive:
@@ -19,11 +24,12 @@ function App(): JSX.Element {
 
   const [messages, setMessages] = useState<message[]>([]);
   const [input, setInput] = useState<string>("");
+  const [openImojiList, setOpenImojiList] = useState<boolean>(false);
 
   useEffect(() => {
     // Listen for chat messages from the server
     socket.on("chat message", (msg: string) => {
-      addMessage(msg , "receive");
+      addMessage(msg, "receive");
     });
 
     scrollToBottom(messageContainer);
@@ -37,24 +43,28 @@ function App(): JSX.Element {
   const handleMessageSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
     if (input.trim() !== "") {
-      addMessage(input , "send");
+      addMessage(input, "send");
       socket.emit("chat message", input); // Send message to server
       setInput("");
     }
   };
 
-  const addMessage = (msg: string , status : string) => {
+  const addMessage = (msg: string, status: string) => {
     const handleMsg: message = { status: status, message: msg };
     setMessages((prevMessages) => [...prevMessages, handleMsg]);
   };
 
-  
+  const handleSelectOmoji = (e:any)=>{
+    setInput((input)=>input+e.native) ; 
+  }
 
   return (
     <div className=" bg-stone-100 h-screen relative flex flex-col justify-between">
       {/* header */}
       <div className="bg-slate-200 py-4 w-full flex items-center justify-center mobile:py-2">
-        <p className="text-4xl tracking-widest font-semibold tablet:text-2xl mobile:text-xl">CHAT APP</p>
+        <p className="text-4xl tracking-widest font-semibold tablet:text-2xl mobile:text-xl">
+          CHAT APP
+        </p>
       </div>
       {/* messages */}
       <div
@@ -62,7 +72,12 @@ function App(): JSX.Element {
         ref={messageContainer}
       >
         {messages.map((msg: message, index: number) => (
-          <div className={`${style[msg.status]} break-words shadow tablet:text-sm mobile:text-xs mobile:px-5`} key={index}>
+          <div
+            className={`${
+              style[msg.status]
+            } break-words shadow tablet:text-sm mobile:text-xs mobile:px-5`}
+            key={index}
+          >
             {msg.message}
           </div>
         ))}
@@ -70,9 +85,16 @@ function App(): JSX.Element {
       {/* form */}
       <div className="w-screen bg-blue-300 py-4 flex justify-center items-center mobile:py-2">
         <form
-          className="flex justify-center items-center gap-10 tablet:gap-5 mobile:gap-5 w-full"
+          className="flex justify-center items-center gap-10 tablet:gap-5 mobile:gap-5 w-full relative"
           onSubmit={handleMessageSubmit}
         >
+          <button
+            type="button"
+            className=" text-3xl text-stone-600 hover:text-black mobile:text-2xl"
+            onClick={() => setOpenImojiList((e) => !e)}
+          >
+            {openImojiList ? <IoCloseSharp /> : <FaRegFaceSmileBeam /> }
+          </button>
           <input
             type="text"
             value={input}
@@ -86,6 +108,11 @@ function App(): JSX.Element {
           >
             <RiSendPlane2Fill />
           </button>
+          {openImojiList && (
+            <div className=" absolute bottom-20 w-[500px] overflow-auto mobile:w-[300px]">
+              <Picker data={data} onEmojiSelect={(e:any)=>handleSelectOmoji(e)} theme="light" />
+            </div>
+          )}
         </form>
       </div>
     </div>
