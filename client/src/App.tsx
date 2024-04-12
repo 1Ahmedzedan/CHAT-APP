6d5e4d8a63ecac6817f159c40deba66abf49a46f
1Ahmedzedan <ugs.141182@ci.suez.edu.eg>
2024-04-12 06:01:54 +0200

    set responsive design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import RegisterPage from "./components/RegisterPage";
 import ChatPage from "./components/ChatPage";
-import MessageContainer from "./components/MessageContainer";
 import ChatStore from "./context/ChatStore";
 
 import { ToastContainer } from "react-toastify";
--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -2,7 +2,7 @@ import { SiKakaotalk } from "react-icons/si";
 
 const Logo = () => {
   return (
-    <div className="text-5xl text-blue-900">
+    <div className="text-5xl text-blue-900 mobile:text-4xl">
       <SiKakaotalk />
     </div>
   );
